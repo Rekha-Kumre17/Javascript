87ab5e6d871dc2c53a96d41031c059446b686692
Rekha-Kumre17 <rekha5kumre@gmail.com>
2024-03-05 15:29:12 +0530

    first commit

---
+++ js.js
@@ -0,0 +1,19 @@
+// let h2 = document.querySelector("h2");
+// console.dir(h2.innerText);
+// h2.innerText = h2.innerText + " from NIT";
+
+// let divs=document.querySelectorAll(".box");
+//  let idx=1;
+// for( div of divs){
+// div.innerText=`New unique value ${idx} `;
+// idx++;}
+
+// let btn = document.createElement("button");
+// console.dir(btn);
+// btn.innerText = "Click Me!";
+// btn.style.color = "white";
+// btn.style.backgroundColor = "red";
+// document.querySelector("body").prepend(btn);
+
+// let par=document.querySelector("p");
+// par.classList.add("newclass");
